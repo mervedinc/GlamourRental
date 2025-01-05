de43d9163703f81278c55c8fbe8f8946ffe93628
mervedinc <merve.dnc44@gmail.com>
2025-01-05 15:24:08 +0300

    Initial commit with project files

--- EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
+++ EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8990dd21ca83596f428359bdbc9edd8595d95e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
